4ba93736797c0283da3b7cc4dac1c8f5ff003e39
hamza hammami <khabibkhabib105@gmail.com>
2025-04-29 17:30:17 +0200

    Changed backend URL to Render

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,5 @@
 import axios from 'axios'
 
 export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
+
+const BASE_URL = "https://mern-backend.onrender.com/api";
